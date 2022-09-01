a9ddac59a26361dbde4ed0505ab349000b1bd1cb
zmlinebaugh04 <zmlinebaugh@students.unwsp.edu>
2022-09-01 18:33:20 -0500

    Inclassweek2
    Creating variables and using math

---
+++ avgheight.java
@@ -0,0 +1,16 @@
+package avgheight;
+
+
+
+public class avgheight {
+	public static void main(String[] args) {
+	int stu1 = 56;
+	int stu2 = 87;
+	int stu3 = 24;
+	int stu4 = 72;
+	int stu5 = 63;
+	int total = stu1 + stu2 + stu3 + stu4 + stu5;
+	float average = total/5;
+	System.out.println("The average height is " + average + " inches.");
+  }
+}
